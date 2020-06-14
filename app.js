a7794b2c35fda314ec6d2fac56aee7b6552778a6
Yasen <362yasen2@gmail.com>
2020-06-14 15:06:25 -0700

    #
    #

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById("binary-form").addEventListener("input", () => {
-  const num = document.getElementById("num").value;
-  const output = document.getElementById("result");
+  const num = document.getElementById("num").value,
+    output = document.getElementById("result");
   output.value = "";
   for (var i = 0; i < num.length; i++) {
     output.value += num[i].charCodeAt(0).toString(2) + " ";
